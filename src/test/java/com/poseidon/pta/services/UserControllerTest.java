bbe349f45b58a3a76dd33073261448321f844b3a
Simon Linford <simon.linford@gmail.com>
2022-02-23 18:10:10 +0100

    Updated UserControllerTest to work with RoleCheck -updated WithMockUser calls to specify username -mock userRepo now responds with correct user object for each mock user -added tests for bad access attempts to confirm only admin can access endpoints

--- src/test/java/com/poseidon/pta/services/UserControllerTest.java
+++ src/test/java/com/poseidon/pta/services/UserControllerTest.java
@@ -1,11 +1,12 @@
 package com.poseidon.pta.services;
 
-import com.poseidon.pta.domain.Trade;
 import com.poseidon.pta.domain.User;
-import com.poseidon.pta.repositories.TradeRepository;
 import com.poseidon.pta.repositories.UserRepository;
+import com.poseidon.pta.security.SecurityUserDetailsService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -46,27 +47,88 @@ public class UserControllerTest {
     @MockBean
     private static UserRepository userRepository;
 
+    @InjectMocks
+    private static UserService userService;
+
+    @Autowired
+    private static SecurityUserDetailsService userDetailsService;
+
+    private static User testAdmin;
+
+    private static User testUser;
+
+    @BeforeAll
+    private static void setUpUser(){
+        testAdmin = new User();
+        testAdmin.setFullname("NameAdmin");
+        testAdmin.setRole("ADMIN");
+        testAdmin.setUsername("UsernameAdmin");
+        testAdmin.setPassword("PASSWORD");
+
+        testUser = new User();
+        testUser.setFullname("NameUser");
+        testUser.setRole("USER");
+        testUser.setUsername("UsernameUser");
+        testUser.setPassword("PASSWORD");
+    }
+
     @Test
-    @WithMockUser
+    @WithMockUser(username="admin")
     public void userControllerGetHomeEndpoint() throws Exception {
+
+        Mockito.when(userRepository.findByUsername("admin")).thenReturn(testAdmin);
 
         MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/user/list").accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
+        //Verify admin user is able to access user list page (code 200)
         assertTrue(mvcResult.getResponse().getStatus() == 200);
+        assertTrue(mvcResult.getResponse().getContentAsString().contains("User List"));
     }
 
     @Test
-    @WithMockUser
+    @WithMockUser(username="user")
+    public void userControllerGetHomeEndpointBadAccess() throws Exception {
+
+        Mockito.when(userRepository.findByUsername("user")).thenReturn(testUser);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/user/list").accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        //Verify non-admin user is forwarded back to home page (code 200)
+        assertTrue(mvcResult.getResponse().getStatus() == 200);
+        assertTrue(mvcResult.getResponse().getContentAsString().contains("HOME PAGE"));
+    }
+
+    @Test
+    @WithMockUser(username="admin")
     public void userControllerGetMappingAddBidForm() throws Exception {
+
+        Mockito.when(userRepository.findByUsername("admin")).thenReturn(testAdmin);
 
         MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/user/add").accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
+        //Verify admin user is able to access add user page (code 200)
         assertTrue(mvcResult.getResponse().getStatus() == 200);
+        assertTrue(mvcResult.getResponse().getContentAsString().contains("Add New User"));
     }
 
     @Test
-    @WithMockUser
+    @WithMockUser(username="user")
+    public void userControllerGetMappingAddBidFormBadAccess() throws Exception {
+
+        Mockito.when(userRepository.findByUsername("user")).thenReturn(testUser);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/user/add").accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        //Verify non-admin user is forwarded back to home page with code (200)
+        assertTrue(mvcResult.getResponse().getStatus() == 200);
+        assertTrue(mvcResult.getResponse().getContentAsString().contains("HOME PAGE"));
+    }
+
+    @Test
+    @WithMockUser(username="admin")
     public void userControllerPostValidateAddsEntry() throws Exception {
+
+        Mockito.when(userRepository.findByUsername("admin")).thenReturn(testAdmin);
 
         MvcResult mvcResult = mockMvc.perform(
                 post("/user/validate")
@@ -83,8 +145,32 @@ public class UserControllerTest {
     }
 
     @Test
-    @WithMockUser
+    @WithMockUser(username="user")
+    public void userControllerPostValidateAddsEntryBadAccess() throws Exception {
+
+        Mockito.when(userRepository.findByUsername("user")).thenReturn(testUser);
+
+        MvcResult mvcResult = mockMvc.perform(
+                post("/user/validate")
+                        .param("fullname", "fullname")
+                        .param("username", "username")
+                        .param("password","TestPassword123$%^")
+                        .param("role", "ADMIN")
+                        .with(csrf())
+                        .accept(MediaType.ALL)).andReturn();
+
+
+        //Verify non-admin user is forwarded back to home page with code (200) and no changes are made to repo
+        Mockito.verify(userRepository, Mockito.times(0)).save(any(User.class));
+        assertTrue(mvcResult.getResponse().getStatus() == 200);
+        assertTrue(mvcResult.getResponse().getContentAsString().contains("HOME PAGE"));
+    }
+
+    @Test
+    @WithMockUser(username="admin")
     public void userControllerGetUpdateForm() throws Exception {
+
+        Mockito.when(userRepository.findByUsername("admin")).thenReturn(testAdmin);
 
         Optional<User> result = Optional.of(new User());
         doReturn(result).when(userRepository).findById(1);
@@ -95,8 +181,26 @@ public class UserControllerTest {
     }
 
     @Test
-    @WithMockUser
+    @WithMockUser(username="user")
+    public void userControllerGetUpdateFormBadAccess() throws Exception {
+
+        Mockito.when(userRepository.findByUsername("user")).thenReturn(testUser);
+
+        Optional<User> result = Optional.of(new User());
+        doReturn(result).when(userRepository).findById(1);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/user/update/1").accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        //Verify non-admin user is forwarded back to home page with code (200)
+        assertTrue(mvcResult.getResponse().getStatus() == 200);
+        assertTrue(mvcResult.getResponse().getContentAsString().contains("HOME PAGE"));
+    }
+
+    @Test
+    @WithMockUser(username="admin",roles={"ADMIN"})
     public void userControllerPostUpdate() throws Exception {
+
+        Mockito.when(userRepository.findByUsername("admin")).thenReturn(testAdmin);
 
         MvcResult mvcResult = mockMvc.perform(
                 post("/user/update/1")
@@ -113,8 +217,31 @@ public class UserControllerTest {
     }
 
     @Test
-    @WithMockUser
+    @WithMockUser(username="user")
+    public void userControllerPostUpdateBadAccess() throws Exception {
+
+        Mockito.when(userRepository.findByUsername("user")).thenReturn(testUser);
+
+        MvcResult mvcResult = mockMvc.perform(
+                post("/user/update/1")
+                        .param("fullname", "fullname")
+                        .param("username", "username")
+                        .param("password","TestPassword123$%^")
+                        .param("role", "ADMIN")
+                        .with(csrf())
+                        .accept(MediaType.ALL)).andReturn();
+
+        //Verify non-admin user is forwarded back to home page with code (200) and no changes are made to repo
+        Mockito.verify(userRepository, Mockito.times(0)).save(any(User.class));
+        assertTrue(mvcResult.getResponse().getStatus() == 200);
+        assertTrue(mvcResult.getResponse().getContentAsString().contains("HOME PAGE"));
+    }
+
+    @Test
+    @WithMockUser(username="admin")
     public void userControllerGetDelete() throws Exception {
+
+        Mockito.when(userRepository.findByUsername("admin")).thenReturn(testAdmin);
 
         Optional<User> result = Optional.of(new User());
         doReturn(result).when(userRepository).findById(1);
@@ -124,6 +251,23 @@ public class UserControllerTest {
         //Verify entry is removed from DB and we are redirected (302)
         assertTrue(mvcResult.getResponse().getStatus() == 302);
         Mockito.verify(userRepository, Mockito.times(1)).delete(any(User.class));
+    }
+
+    @Test
+    @WithMockUser(username="user")
+    public void userControllerGetDeleteBadAccess() throws Exception {
+
+        Mockito.when(userRepository.findByUsername("user")).thenReturn(testUser);
+
+        Optional<User> result = Optional.of(new User());
+        doReturn(result).when(userRepository).findById(1);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/user/delete/1").accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        //Verify non-admin user is forwarded back to home page with code (200) and no changes are made to repo
+        Mockito.verify(userRepository, Mockito.times(0)).delete(any(User.class));
+        assertTrue(mvcResult.getResponse().getStatus() == 200);
+        assertTrue(mvcResult.getResponse().getContentAsString().contains("HOME PAGE"));
     }
 
 
